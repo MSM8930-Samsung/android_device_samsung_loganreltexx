00abcafe0d4c7ad210c22f6bf17b8c8769486456
Raghavendra Prasad Nagaraj <prasr@codeaurora.org>
2015-11-04 12:54:18 +0530

    Enable delay service restart
    This patch turns on system property "ro.am.reschedule_service".
    This helps to postpone services restarts during app launch.
    Only the services that are not relevant to current app launch are
    considered for postpone.
    
    Change-Id: I908be6dd7f7991b830a0440276fb9e2a0c0314c3
    
    # Conflicts:
    #	system_prop.mk

--- system_prop.mk
+++ system_prop.mk
@@ -61,6 +61,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-swap=false \
+    ro.am.reschedule_service=true \
     ro.sys.fw.dex2oat_thread_count=2 \
     dalvik.vm.dex2oat-Xms=64m \
     dalvik.vm.dex2oat-Xmx=384m
