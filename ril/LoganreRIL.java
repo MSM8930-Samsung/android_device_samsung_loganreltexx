2c276cc860c4b49b007e3009ae37967349f1038d
Arne Coucheron <arco68@gmail.com>
2015-11-05 00:01:27 +0100

    Revert "Revert "ril: Add support for 4.4.4 RIL""
     * We're using LP rild/libril in M, so I guess we need this
    
    This reverts commit 2ce418d6e13dc4085e18b4faf507b326bf4bb11e.
    
    Change-Id: Ic454feae063b874c5183c7a7f827c86fb9027a1b

--- ril/LoganreRIL.java
+++ ril/LoganreRIL.java
@@ -319,4 +319,15 @@ public class LoganreRIL extends RIL {
             response.sendToTarget();
         }
     }
+
+    @Override
+    public void
+    acceptCall (Message result) {
+        RILRequest rr
+        = RILRequest.obtain(RIL_REQUEST_ANSWER, result);
+        if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest));
+        rr.mParcel.writeInt(1);
+        rr.mParcel.writeInt(0);
+        send(rr);
+    }
 }
