6e4232a398f43d52f1c4bee12dee2f50a934b48b
Arne Coucheron <arco68@gmail.com>
2015-11-28 02:17:56 +0100

    ril: Update namePresentation for LP blobs
    Change-Id: I077a228c4e46c7cf1defe7ffdbb918177dc32815

--- ril/LoganreRIL.java
+++ ril/LoganreRIL.java
@@ -153,7 +153,7 @@ public class LoganreRIL extends RIL {
             int np = p.readInt();
             dc.numberPresentation = DriverCall.presentationFromCLIP(np);
             dc.name = p.readString();
-            dc.namePresentation = p.readInt();
+            dc.namePresentation = DriverCall.presentationFromCLIP(p.readInt());
             int uusInfoPresent = p.readInt();
             if (uusInfoPresent == 1) {
                 dc.uusInfo = new UUSInfo();
