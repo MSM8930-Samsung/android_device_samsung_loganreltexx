a7e6bd1916abfa227069a73bbd5b7b7b7dca1e72
Arne Coucheron <arco68@gmail.com>
2015-12-06 08:25:16 +0100

    ril: Revert changes for LP blobs
     * We're able to use stock blobs now
    
    This reverts commit 07ab218bc1ad92f818881a07ec1fcf3934886a79.
    
    Change-Id: Id3f4f041842037fb0f917679f5ab96c9992544a7

--- ril/LoganreRIL.java
+++ ril/LoganreRIL.java
@@ -37,9 +37,12 @@ import java.util.Collections;
  */
 public class LoganreRIL extends RIL {
 
-    private static final int RIL_UNSOL_RESPONSE_IMS_NETWORK_STATE_CHANGED = 1037;
+    private static final int RIL_REQUEST_DIAL_EMERGENCY = 10016;
+    private static final int RIL_UNSOL_RESPONSE_IMS_NETWORK_STATE_CHANGED = 1036;
     private static final int RIL_UNSOL_DEVICE_READY_NOTI = 11008;
     private static final int RIL_UNSOL_AM = 11010;
+    private static final int RIL_UNSOL_WB_AMR_STATE = 11017;
+    private static final int RIL_UNSOL_RESPONSE_HANDOVER = 11021;
 
 
     public LoganreRIL(Context context, int networkModes, int cdmaSubscription) {
@@ -54,6 +57,10 @@ public class LoganreRIL extends RIL {
     @Override
     public void
     dial(String address, int clirMode, UUSInfo uusInfo, Message result) {
+        if (PhoneNumberUtils.isEmergencyNumber(address)) {
+            dialEmergencyCall(address, clirMode, result);
+            return;
+        }
 
         RILRequest rr = RILRequest.obtain(RIL_REQUEST_DIAL, result);
 
@@ -145,6 +152,7 @@ public class LoganreRIL extends RIL {
             dc.als = p.readInt();
             voiceSettings = p.readInt();
             dc.isVoice = (0 == voiceSettings) ? false : true;
+            boolean isVideo = (0 != p.readInt());   // Samsung CallDetails
             int call_type = p.readInt();            // Samsung CallDetails
             int call_domain = p.readInt();          // Samsung CallDetails
             String csv = p.readString();            // Samsung CallDetails
@@ -153,7 +161,7 @@ public class LoganreRIL extends RIL {
             int np = p.readInt();
             dc.numberPresentation = DriverCall.presentationFromCLIP(np);
             dc.name = p.readString();
-            dc.namePresentation = DriverCall.presentationFromCLIP(p.readInt());
+            dc.namePresentation = p.readInt();
             int uusInfoPresent = p.readInt();
             if (uusInfoPresent == 1) {
                 dc.uusInfo = new UUSInfo();
@@ -238,6 +246,12 @@ public class LoganreRIL extends RIL {
             case RIL_UNSOL_AM:
                 ret = responseString(p);
                 break;
+            case RIL_UNSOL_WB_AMR_STATE:
+                ret = responseInts(p);
+                break;
+            case RIL_UNSOL_RESPONSE_HANDOVER:
+                ret = responseVoid(p);
+                break;
             default:
                 // Rewind the Parcel
                 p.setDataPosition(dataPosition);
@@ -259,6 +273,23 @@ public class LoganreRIL extends RIL {
             AsyncResult.forMessage(result, null, ex);
             result.sendToTarget();
         }
+    }
+
+    private void
+    dialEmergencyCall(String address, int clirMode, Message result) {
+        RILRequest rr;
+
+        rr = RILRequest.obtain(RIL_REQUEST_DIAL_EMERGENCY, result);
+        rr.mParcel.writeString(address);
+        rr.mParcel.writeInt(clirMode);
+        rr.mParcel.writeInt(0);        // CallDetails.call_type
+        rr.mParcel.writeInt(3);        // CallDetails.call_domain
+        rr.mParcel.writeString("");    // CallDetails.getCsvFromExtra
+        rr.mParcel.writeInt(0);        // Unknown
+
+        if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest));
+
+        send(rr);
     }
 
     /**
@@ -287,16 +318,5 @@ public class LoganreRIL extends RIL {
             AsyncResult.forMessage(response, null, ex);
             response.sendToTarget();
         }
-    }
-
-    @Override
-    public void
-    acceptCall (Message result) {
-        RILRequest rr
-        = RILRequest.obtain(RIL_REQUEST_ANSWER, result);
-        if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest));
-        rr.mParcel.writeInt(1);
-        rr.mParcel.writeInt(0);
-        send(rr);
     }
 }
