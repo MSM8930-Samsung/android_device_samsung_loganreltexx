ac4074b31a52d852a7c9bbfeaecd8a36bd341540
kostoulhs <MrKostoulhs@gmail.com>
2016-07-26 12:42:16 +0200

    slimify

--- slim.mk
+++ slim.mk
@@ -1,12 +1,12 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Enhanced NFC
-#$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+#$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/samsung/loganreltexx/full_loganreltexx.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=loganreltexx TARGET_DEVICE=loganrelte BUILD_FINGERPRINT="samsung/loganreltexx/loganrelte:4.2.2/JDQ39/S7275RXXUANG1:user/release-keys" PRIVATE_BUILD_DESC="loganreltexx-user 4.2.2 JDQ39 S7275RXXUANG1 release-keys"
 
 PRODUCT_DEVICE := loganreltexx
-PRODUCT_NAME := cm_loganreltexx
+PRODUCT_NAME := slim_loganreltexx
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo cm_loganreltexx-userdebug
+add_lunch_combo slim_loganreltexx-userdebug
