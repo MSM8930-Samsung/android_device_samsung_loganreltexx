6402bb35aaa7a33b899bda41e6d47fb9730d0136
kostoulhs <MrKostoulhs@gmail.com>
2016-07-27 02:04:36 +0200

    some reverts

--- cmhw/org/cyanogenmod/hardware/SerialNumber.java
+++ cmhw/org/cyanogenmod/hardware/SerialNumber.java
@@ -17,7 +17,7 @@
 package org.cyanogenmod.hardware;
 
 import android.os.SystemProperties;
-import org.cyanogenmod.internal.util.FileUtils;
+import org.cyanogenmod.hardware.util.FileUtils;
 
 /**
  * Support for displaying a different serial number in Settings -> About Phone
