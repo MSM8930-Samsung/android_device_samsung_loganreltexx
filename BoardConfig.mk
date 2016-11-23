5e9e69b4c0f52e4b5554dd997d65c44333eb899a
kostoulhs <MrKostoulhs@gmail.com>
2016-11-23 11:29:18 +0100

    Revert "common: Enable kernel exFAT"
    This reverts commit 0e25bc1443f0f2310680b9bdff984c0fe017698c.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,7 +32,6 @@ BOARD_KERNEL_BASE := 0x80200000
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 BOARD_KERNEL_PAGESIZE := 2048
 TARGET_KERNEL_SOURCE := kernel/samsung/msm8930-common
-TARGET_KERNEL_HAVE_EXFAT := true
 TARGET_KERNEL_CONFIG := cyanogenmod_loganre_defconfig
 TARGET_KERNEL_VARIANT_CONFIG := msm8930_loganre_eur_lte_defconfig
 
