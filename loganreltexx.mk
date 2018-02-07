cd232a903e3361aaa89f57bc74b7a45065aa0d2b
Ethan Chen <intervigil@gmail.com>
2018-02-07 07:19:45 +0100

    loganre: Add native binderized vibrator HAL

--- loganreltexx.mk
+++ loganreltexx.mk
@@ -131,7 +131,7 @@ PRODUCT_PACKAGES += \
     
 # Vibrator 
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-service.serrano
 
 # USB
 PRODUCT_PACKAGES += \
